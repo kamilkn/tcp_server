c7e7e8f3906d94f19b89bd38d66f63de4a174a15
kamilkn <61585695+kamilkn@users.noreply.github.com>
2023-11-03 09:28:52 +0300

    улучшено логирование

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,51 @@
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{self, AsyncWriteExt, AsyncReadExt};
+use log::{error, info};
 use rand::Rng;
-use sha2::{Sha256, Digest};
-use log::log;
+use sha2::{Digest, Sha256};
+use simple_logger::SimpleLogger;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    SimpleLogger::new().init().unwrap();
     let listener = TcpListener::bind("127.0.0.1:8084").await?;
     loop {
         let (socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
             match handle_connection(socket).await {
-                Ok(_) => println!("Connection handled successfully"),
-                Err(e) => eprintln!("Error handling connection: {:?}", e),
+                Ok(_) => info!("Connection handled successfully"),
+                Err(e) => error!("Error handling connection: {:?}", e),
             }
         });
-
     }
 }
 
 async fn handle_connection(mut socket: TcpStream) -> io::Result<()> {
-  let (random_string, challenge) = generate_challenge();
-  println!("Challenge: {}", challenge);
-  
+    let (random_string, challenge) = generate_challenge();
 
-  socket.write_all(challenge.as_bytes()).await?;
+    info!("Challenge: {}", challenge);
 
-  let nonce = read_nonce(&mut socket).await?;
-  
-  if verify_proof(&random_string, &nonce) {
-      println!("PoW verified");
-      let quote = get_random_quote();
-      socket.write_all(quote.as_bytes()).await?;
-  } else {
-      println!("PoW verification failed");
-  }
+    socket.write_all(challenge.as_bytes()).await?;
 
-  Ok(())
+    let nonce = read_nonce(&mut socket).await?;
+
+    if verify_proof(&random_string, &nonce) {
+        info!("PoW verified");
+        let quote = get_random_quote();
+        socket.write_all(quote.as_bytes()).await?;
+    } else {
+        error!("PoW verification failed");
+    }
+
+    Ok(())
 }
 
 async fn read_nonce(socket: &mut TcpStream) -> io::Result<String> {
-  let mut buf = [0; 1024];
-  let n = socket.read(&mut buf).await?;
-  Ok(String::from_utf8_lossy(&buf[..n]).to_string())
+    let mut buf = [0; 1024];
+    let n = socket.read(&mut buf).await?;
+    Ok(String::from_utf8_lossy(&buf[..n]).to_string())
 }
-
 
 fn generate_challenge() -> (String, String) {
     let random_string: String = rand::thread_rng()
@@ -53,16 +53,16 @@ fn generate_challenge() -> (String, String) {
         .take(10)
         .map(char::from)
         .collect();
-    let challenge = format!("{}:{}", random_string, "000");  // 3 leading zeros as an example
+    let challenge = format!("{}:{}", random_string, "000"); // 3 leading zeros as an example
     (random_string, challenge)
 }
 
 fn verify_proof(random_string: &str, nonce: &str) -> bool {
     let data = format!("{}{}", random_string, nonce);
     let hash = Sha256::digest(data.as_bytes());
-    hash.starts_with(b"000")  // Assuming the requirement is 3 leading zeros
+    hash.starts_with(b"000") // Assuming the requirement is 3 leading zeros
 }
 
 fn get_random_quote() -> String {
-  "The only true wisdom is in knowing you know nothing.".to_string()  // Placeholder
+    "The only true wisdom is in knowing you know nothing.".to_string() // Placeholder
 }
